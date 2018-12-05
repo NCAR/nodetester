f4755ec15b7a4b24ff911d0b6af0c402424d95a3
Brian Vanderwende <vanderwb@ucar.edu>
2018-12-05 13:30:15 -0700

    Added tests for old and new DAV

--- run_wrf_dav.sh
+++ run_wrf_dav.sh
@@ -5,11 +5,19 @@
 #   Run a short 149 timestep WRF restart simulation.
 #
 #	Maintainer: Brian Vanderwende
-#	Revised:    10:07, 06 Sep 2018
+#	Revised:    09:13, 04 Oct 2018
 
 # Test configuration
 CASE=wrf
 ACCOUNT=SCSG0001
+CNC=$(sinfo -N -t IDLE | grep casper | wc -l)
+ONC=$(sinfo -N -t IDLE | grep -e geyser -e caldera -e pronghorn | wc -l)
 
-# Run nodetester
-./nodetester --machine dav --queue system --account $ACCOUNT $CASE
+# Run nodetester across all nodes
+#./nodetester --machine dav --queue system --account $ACCOUNT $CASE
+
+# Run nodetester on Casper
+#./nodetester --machine dav --queue system --options "-C casper" --nodes $CNC --account $ACCOUNT $CASE
+
+# Run nodetester on old DAV (geyser, caldera, pronghorn)
+#./nodetester --machine dav --queue system --options "-C geyser|caldera|pronghorn" --nodes $ONC --account $ACCOUNT $CASE
